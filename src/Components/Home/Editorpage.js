56a52763098c0144269230fd8dd66c25a4a81604
t4shar <tushar.gandhi0092@gmail.com>
2022-07-19 22:51:11 +0530

    Completely finalize the editor page done with the output and input area

--- src/Components/Home/Editorpage.js
+++ src/Components/Home/Editorpage.js
@@ -3,59 +3,74 @@ import Navbar from "../Navbar";
 import Editor from "@monaco-editor/react";
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
-import {compileCode} from '../../Api/index'
+import { compileCode } from '../../Api/index'
+var qs = require("qs");
 
 function Editorpage() {
   const [setcodemark, setsetcodemark] = useState("//Write your code below");
   const [langval, setlangval] = useState("langauge");
+  const [inputarea, setinputarea] = useState('')
+  const [outputarea, setoutputarea] = useState('')
   const editorref = useRef(null);
 
   const handleEditorDidMount = (editor, monaco) => {
     editorref.current = editor;
   };
   // exectue the code
-  const handleuprun = async (e)=>{
-      const data = {
-        "code":setcodemark,
-        "language":langval,
-        "input":""
+  const handleuprun = async (e) => {
+    setoutputarea('');
+    const data = {
+      "code": setcodemark,
+      "language": langval,
+      "input": inputarea
+    }
+    console.log(data);
+    var res = await compileCode(data);
+    console.log(res);
+    // const finalres = JSON.stringify(res.data)
+    
+      if(res.data.success === true){
+        setoutputarea(res.data.output);
+      }else{
+        setoutputarea(res.data.error)
       }
-      console.log(data);
-      var res =  await compileCode(data);
-      console.log(JSON.stringify(res.data));
   }
-  const setlangforeditor = ()=>{
-    if(langval == 'py') return "python"
-    if(langval == 'cpp') return "c++"
+
+  const setlangforeditor = () => {
+    if (langval == 'py') return "python"
+    if (langval == 'cpp') return "c++"
     return 'java'
   }
-  const handleuplang = (e)=>{
+  const handleuplang = (e) => {
     setlangval(e);
     console.log(langval)
   }
   const handleupsave = () => {
     console.log(editorref.current.getValue());
   };
+  const handleupinputchange = (event)=>{
+    setinputarea(event.target.value);
+    console.log(inputarea)
+  }
   return (
     <>
       <Navbar langval={langval} setlangval={setlangval} />
       <div className="editorpagewrapper">
         <div class=" row row-cols-2">
-          <div class="col editorgp">
+          <div class="col editorgp p-0">
             <div className="row row-cols-2">
-            <div className="col">
+              <div className="col">
 
-          <DropdownButton id="dropdown-basic-button"  onSelect={handleuplang} title={langval}>
-        <Dropdown.Item onSelect={handleuplang} value="cpp" eventKey="cpp">C++</Dropdown.Item>
-        <Dropdown.Item onSelect={handleuplang} value="java" eventKey="java">JAVA</Dropdown.Item>
-         <Dropdown.Item onSelect={handleuplang} eventKey="py">Python</Dropdown.Item>
-    </DropdownButton>
-            </div>
-            <div className="col">
-            <button className="btn btn-primary policy" onClick={handleupsave}>Save</button>
-            <a className="btn btn-primary" onClick={handleuprun}>RUN</a>
-    
-            </div>
+                <DropdownButton id="dropdown-basic-button" className="mx-3" onSelect={handleuplang} title={langval}>
+                  <Dropdown.Item onSelect={handleuplang} value="cpp" eventKey="cpp">C++</Dropdown.Item>
+                  <Dropdown.Item onSelect={handleuplang} value="java" eventKey="java">JAVA</Dropdown.Item>
+                  <Dropdown.Item onSelect={handleuplang} eventKey="py">Python</Dropdown.Item>
+                </DropdownButton>
+              </div>
+              <div className="col">
+                <button className="btn btn-primary policy" onClick={handleupsave}>Save</button>
+                <a className="btn btn-primary" onClick={handleuprun}>RUN</a>
+              </div>
             </div>
             <Editor
               height="100%"
@@ -63,10 +78,29 @@ function Editorpage() {
               defaultLanguage='java'
               theme="vs-dark"
               onChange={(value) => setsetcodemark(value)}
-              onMount={handleEditorDidMount }
+              onMount={handleEditorDidMount}
             />
           </div>
-          <div class="col">Column</div>
+          <div class="d-flex flex-column p-0">
+            <div class="p-2 ggi" style={{ color: 'white' }}>
+              <h4 className="center">Write your input below</h4>
+              <div class="md-form">
+  <textarea id="form7" class="md-textarea form-control" value={inputarea} 
+  style={{backgroundColor:'rgb(44, 39, 39)',color : 'white'}}
+  onChange={handleupinputchange} rows="10"></textarea>
+</div>
+
+            </div>
+            <div class="p-2 ggi" style={{ color: 'white' }}>
+              <h4 className="center">Your Output Terminal</h4>
+              <div class="md-form">
+  <textarea id="form7" class="md-textarea form-control" value={outputarea} 
+  style={{backgroundColor:'rgb(44, 39, 39)',color : 'white'}}
+  onChange={handleupinputchange} rows="10"></textarea>
+</div>
+            </div>
+          </div>
+
         </div>
       </div>
     </>
