6c1726dd8e231c56da6bfdf3b333363340e448da
t4shar <tushar.gandhi0092@gmail.com>
2022-07-19 13:50:06 +0530

    Code Editor done before adding compile code api

--- src/Components/Home/Editorpage.js
+++ src/Components/Home/Editorpage.js
@@ -1,19 +1,59 @@
-import React from 'react'
-import Navbar from '../Navbar'
-import Editor from './Editor'
+import React, { useRef, useState } from "react";
+import Navbar from "../Navbar";
+import Editor from "@monaco-editor/react";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Button  from 'react-bootstrap/Button';
+
 function Editorpage() {
+  const [setcodemark, setsetcodemark] = useState("//Write your code below");
+  const [langval, setlangval] = useState("langauge");
+  const editorref = useRef(null);
+
+  const handleEditorDidMount = (editor, monaco) => {
+    editorref.current = editor;
+  };
+  const handleuplang = (e)=>{
+    setlangval(e);
+    console.log(langval)
+  }
+  const handleupsave = () => {
+    console.log(editorref.current.getValue());
+  };
   return (
     <>
-    <Navbar/>
-    <div  style={{color : "white"}} class=" center
-      row row-cols-2">
-    <div  class="col bg-white editorgp">
-        <Editor/>
-    </div>
-    <div class="col">Column</div>
-  </div>
-        </>
-  )
+      <Navbar langval={langval} setlangval={setlangval} />
+      <div className="editorpagewrapper">
+        <div class=" row row-cols-2">
+          <div class="col editorgp">
+            <div className="row row-cols-2">
+            <div className="col">
+
+          <DropdownButton id="dropdown-basic-button"  onSelect={handleuplang} title={langval}>
+        <Dropdown.Item onSelect={handleuplang} value="C++" eventKey="C++">C++</Dropdown.Item>
+        <Dropdown.Item onSelect={handleuplang} value="Java" eventKey="java">JAVA</Dropdown.Item>
+         <Dropdown.Item onSelect={handleuplang} eventKey="Python">Python</Dropdown.Item>
+    </DropdownButton>
+            </div>
+            <div className="col">
+            <button className="btn btn-primary policy" onClick={handleupsave}>Save</button>
+    
+            </div>
+            </div>
+            <Editor
+              height="100%"
+              value={setcodemark}
+              theme="vs-dark"
+              onChange={(value) => setsetcodemark(value)}
+              defaultLanguage="javascript"
+              onMount={handleEditorDidMount }
+            />
+          </div>
+          <div class="col">Column</div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Editorpage
+export default Editorpage;
